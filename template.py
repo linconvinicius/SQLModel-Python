32420cef542a2f8af88ade4c900595f9fbe63549
linconvinicius <lvini_@live.com>
2025-03-21 16:56:48 -0300

    create session and statement

--- template.py
+++ template.py
@@ -1,5 +1,11 @@
 from app import Usuario, engine
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 with Session(engine) as session:
     usuario = Usuario(nome="Roberval", idade=32, email="teste@gmail.com")
+    session.add(usuario)
+    session.commit()
+
+    statement = select(Usuario)
+    results = session.exec(statement).all()
+    print(results)
